733fde93ed84e18e868ae87eacf2e042d92cf483
Stasio <stachowicz.karol@gmail.com>
2020-05-28 14:59:39 +0200

    All changes by me

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
+def hello_world():
     return render_template('index.html')
+
+@app.route('/cartridge')
+def cartridge():
+    return render_template('cartridge.html')
+
+
+@app.route('/run_game')
+def run_game():
+    return render_template('run_game.html')
 
 
 if __name__ == '__main__':
